c252740f9b166a44d9caa35c6d92e403adceed85
andrey <andrey@moshbear.net>
2025-08-13 00:37:43 -0400

    zipForEach: add minor clarifications

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.moshy.containers"
-version = "0.7.0"
+version = "0.7.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/IterableZipForeach.kt
+++ src/main/kotlin/IterableZipForeach.kt
@@ -2,13 +2,19 @@ package com.moshy.containers
 
 /**
  * Like `a.zip(b) { a, b -> ... }` but doesn't produce a `List<Unit>`.
- * @see zip
+ *
+ * Trivia: one could implement [Iterable.zip] with this via a construction like
+ * ```
+ *  fun <T, U> Iterable<T>.zip(other: Iterable<U>) =
+ *      buildList { zipForEach(other) { a, b -> add(a to b) } }
+ *  ```
+ * @see [Iterable.zip]
  * @see forEach
  */
 
-inline fun <T, U> Iterable<T>.zipForEach(other: Iterable<U>, transform: (a: T, b: U) -> Unit) {
+inline fun <T, U> Iterable<T>.zipForEach(other: Iterable<U>, action: (a: T, b: U) -> Unit) {
     val first = iterator()
     val second = other.iterator()
     while (first.hasNext() && second.hasNext())
-        transform(first.next(), second.next())
+        action(first.next(), second.next())
 }
